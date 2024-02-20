7389973004940687b2a9de4c3b8b0aa8a2b6080d
Deepak Chauhan <deepak858783@gmail.com>
2024-02-20 22:13:53 +0530

    final
    Project made from freeCodeCamp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState(
+    "If you want to lift yourself up, lift up someone else."
+  );
+  const [author, setAuthor] = useState("Booker T. Washington");
+
+  const getQuote = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let url = "https://type.fit/api/quotes";
+    const response = await fetch(url);
+    const allQuotes = await response.json();
+    const indx = Math.floor(Math.random() * allQuotes.length);
+    const qresult = allQuotes[indx].text;
+    const auth = allQuotes[indx].author;
+    const filterAuth = auth.replace(", type.fit", "");
+    setQuote(qresult);
+    setAuthor(filterAuth);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box">
+      <p id="text">{quote}</p>
+      <p id="author">~{author}</p>
+      <div id="btns">
+        <button type="button" className="btn">
+          <a id="tweet-quote" href="twitter.com/intent/tweet">
+            Tweet
+          </a>
+        </button>
+        <button type="button" className="btn" id="new-quote" onClick={getQuote}>
+          New Quote
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
